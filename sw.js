dfdc870bfd993ba26516fa5b3a1492c613dde2be
Remko Roodselaar <R.Roodselaar@outlook.com>
2019-03-27 22:18:10 +0100

    Step 3 - npm run-script build creates service worker with workbox

--- sw.js
+++ sw.js
@@ -1,20 +1,45 @@
-// first run
-self.addEventListener("install", event => {
-  // prevent stopping before the cache is set
-  event.waitUntil(
-    caches.open("precache-v1").then(cache => {
-      // add response at "/" and "app.js" to cache
-      cache.addAll(["/", "app.js"]);
-    })
-  );
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
-// intercept the fetch event
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      // first serve from cache then fetch online
-      return response || fetch(event.request);
-    })
-  );
-});
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "app.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "app.js",
+    "revision": "68b329da9893e34099c7d8ad5cb9c940"
+  },
+  {
+    "url": "index.html",
+    "revision": "546e8bc5e0f4eed03999d1292eb59cef"
+  },
+  {
+    "url": "workbox-config.js",
+    "revision": "2a5130c5c0adbb75c11ff6636f008fb6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
