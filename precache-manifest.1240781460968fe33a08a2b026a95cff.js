9565cf9e5bc60e549aca5a11c207467186e150b9
RRoodselaar <rroodselaar@hotmail.com>
2020-08-14 02:50:21 +0200

    Deploy updates

---
+++ precache-manifest.1240781460968fe33a08a2b026a95cff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185e80b6f29c24dbbdbc958259458c9a",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "d189205048d6020c14f77007a759aa1a",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "89ade1980b46393f4a82",
+    "url": "app.js"
+  },
+  {
+    "revision": "0cf64f21d6f2ae220c22388f7bb2f9b3",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "0b56c5c0daa1d3e9593995b157797d6d",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "28635efd2f68216f6f66745e393d3d1d",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "6086db415c325bccb7953f2d5b526149",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "be56e0b35cd07dcba0e0606ac55522db",
+    "url": "index.html"
+  },
+  {
+    "revision": "141cdc5ac99c863954bfdbdad9e42602",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "204b2c1756f8704dba979f572b6e66df",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "3375e19d42b328fa5806f373ea933b95",
+    "url": "safari-pinned-tab.svg"
+  }
+]);
