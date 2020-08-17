dc61140025c252ee10fa4032cfd042f157e2fe9c
RRoodselaar <rroodselaar@hotmail.com>
2020-08-17 19:00:12 +0200

    Inject environment settings into build script

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,37 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: "production",
-  entry: "./src/app.js",
-  output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ["**/*", "!.git/**"]
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "./src/index.html",
-          to: "index.html"
-        },
-        {
-          from: "./src/manifest.json",
-          to: "manifest.json"
-        },
-        "./src/assets"
-      ]
-    }),
-    new WorkboxWebpackPlugin.InjectManifest({
-      swSrc: "./src/src-sw.js",
-      swDest: "sw.js"
-    })
-  ]
+module.exports = env => {
+  const mode = env.mode ? env.mode : "production";
+
+  return {
+    mode,
+    entry: "./src/app.js",
+    output: {
+      filename: "app.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: ["**/*", "!.git/**"]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "./src/index.html",
+            to: "index.html"
+          },
+          {
+            from: "./src/manifest.json",
+            to: "manifest.json"
+          },
+          "./src/assets"
+        ]
+      }),
+      new WorkboxWebpackPlugin.InjectManifest({
+        swSrc: "./src/src-sw.js",
+        swDest: "sw.js"
+      })
+    ]
+  }
 };
